2fc570f47e5a98ac9ce2bc0c007829e96f20e49e
Muhammad Muazzain <90068510+MuhammadMuazzain@users.noreply.github.com>
2025-09-09 01:18:42 +0500

    Mine

--- notebook/download_10k.py
+++ notebook/download_10k.py
@@ -25,6 +25,7 @@ def download_10k(url, save_path):
     # response = requests.get(url, headers=headers)
     # response = requests.get(url, headers=headers)
     # response = requests.get(url, headers=headers)
+    # response = requests.get(url, headers=headers)
     response = requests.get(url, headers=headers)
     
 
